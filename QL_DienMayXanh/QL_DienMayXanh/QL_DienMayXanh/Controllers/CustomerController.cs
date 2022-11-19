d6ca3e0f61a7653306b4ff1f6afcaf3e29076b69
MinhHuy <minhhuyduong7@gmail.com>
2022-11-19 19:45:17 +0700

    fist_commit

---
+++ QL_DienMayXanh/QL_DienMayXanh/QL_DienMayXanh/Controllers/CustomerController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace QL_DienMayXanh.Controllers
+{
+    public class CustomerController : Controller
+    {
+        public IActionResult CustomerHome()
+        {
+            return View();
+        }
+        public IActionResult Product()
+        {
+            return View();
+        }
+    }
+}
